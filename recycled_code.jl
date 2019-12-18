ca9dd644419df0ba79624cca338c452a4d421545
Gaspar Faure <gasparfaure@hotmail.com>
2019-12-18 17:24:52 -0500

    Moving pca to recycled code

--- recycled_code.jl
+++ recycled_code.jl
@@ -43,3 +43,97 @@ for i = 1:length(seuils)
 end
 
 [meilleur_seuil meilleur_f1]
+
+
+"""
+getForestPCA permet d'obtenir pour chaque ouvrage un modèle de prédiction basé sur l'analyse en composante principale et 
+sur la classification par forêts aléatoires. Les paramètres pca_fit_params et rf_fit_params correspondent aux valeurs 
+possibles des hyperparamètres des modèles PCA et RandomForestClassifier que nous utilisons.
+
+@params
+num_ouvrage:      le numéro de l'ouvrage à évaluer
+pca_fit_param:    dictionnaire des valeurs possibles pour les hyperparamètres du modèle PCA
+rf_fit_param:     dictionnaire des valeurs possibles pour les hyperparamètres du modèle RF
+n_folds:          le nombre de plis à effectuer sur l'ensemble d'entrainement lors de la validation croisée
+grid:             false par défaut, si à true, la fonction effectue une GridSearch (exhaustive) sur l'ensemble des
+                  hyperparamètres fournis, sinon elle effectue une RandomizedSearch
+n_iter:           10 par défaut, le nombre de modèles à essayer lors d'une RandomizedSearch
+
+"""
+
+
+function getForestPCA(num_ouvrage::String; pca_fit_params::Dict, rf_fit_params::Dict, n_folds = 5, grid=false, n_iter = 10, verbose=1)
+    train = CreateDataframe(num_ouvrage, surverse_df)
+    y = train[!, :SURVERSE]
+    X = convert(Matrix{Float64}, train[:, 4:28])
+    
+    # Analyse en composantes principales.
+    # Le nombre de composantes principales est également testé dans la GridSearch/RandomizedSearch
+    pca = PCA()
+    fit!(pca, X)
+
+    # Préparation de la classification par RandomForests.
+    # Les HyperParamètres seront évalués dans la GridSearch/RandomizedSearch
+    rf = RandomForestClassifier()
+    
+    # Préparation du Pipeline à tester.
+    # Le Pipeline permet de lier l'obtention des composantes principales à la création d'une RandomForest les utilisant.
+    pipe = Pipelines.Pipeline([("pca", pca), ("rf", rf)])
+    
+    n_models = 1
+
+    fit_params = Dict()
+    for (k,v) in pca_fit_params
+        fit_params["pca__$(k)"] = v
+        n_models *= length(v)
+    end
+    
+    for (k,v) in rf_fit_params
+        fit_params["rf__$(k)"] = v
+        n_models *= length(v)
+    end
+
+    println("Nombre de modèles à parcourir: $(n_models)\n")
+
+    if grid
+        M = GridSearch.GridSearchCV(estimator=pipe,
+                                    param_grid=fit_params,
+                                    cv=n_folds, 
+                                    verbose=verbose)
+    else
+        M = GridSearch.RandomizedSearchCV(estimator=pipe,
+                                          param_distributions=fit_params,
+                                          cv=n_folds,
+                                          n_iter=n_iter, 
+                                          verbose=verbose)
+    end
+
+    return fit!(M, X, y)
+end
+
+ouvrage = ["3260-01D", "3350-07D", "4240-01D", "4350-01D", "4380-01D"]
+best_models = Dict()
+
+#Préparation d'un dictionnaire de paramètres pour l'optimisation des hyperparamètres du modèle
+#On déclare les valeurs jugées possibles pour le modèle
+
+pca_fit_params = Dict()
+pca_fit_params["n_components"] = convert(Array{Int64}, 13)
+
+rf_fit_params = Dict()
+rf_fit_params["n_subfeatures"] = convert(Array{Int64}, 3:1:5)#sans pca, 3:1:10
+rf_fit_params["n_trees"] = convert(Array{Int64}, 80:5:200)#300
+#rf_fit_params["partial_sampling"] = [0.6, 0.7, 0.8]
+rf_fit_params["max_depth"] = convert(Array{Int64}, 4:1:20)#50
+#rf_fit_params["min_samples_leaf"] = convert(Array{Int64}, 5:2:10)
+#rf_fit_params["min_samples_split"] = convert(Array{Int64}, 2:2:16)
+#rf_fit_params["min_purity_increase"] = [0.0, 0.1, 0.2]
+
+for i=1:length(ouvrage)
+    @time best_models[ouvrage[i]] = getForestPCA(ouvrage[i],
+                                                 pca_fit_params = pca_fit_params,
+                                                 rf_fit_params = rf_fit_params,
+                                                 n_iter=1000)
+    println(ouvrage[i], " done")
+    println(best_models[ouvrage[i]].best_score_, "\n")
+end
